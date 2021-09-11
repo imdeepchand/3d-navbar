91991d7c831d623937c31b34b42b224faa543a4c
imdeepchand <imdeepchanddewangan@gmail.com>
2021-09-11 16:43:31 +0530

    upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const cls = document.querySelector('.clc-btn')
+const hum = document.querySelector('.hum-btn')
+const navbar = document.querySelector('.navbar')
+const main = document.querySelector('.main')
+const a = document.querySelectorAll('a')
+
+hum.addEventListener("click", () => {
+    navbar.classList.add('active_navbar');
+    main.classList.add('active_main');
+})
+
+cls.addEventListener("click", () => {
+    navbar.classList.remove('active_navbar');
+    main.classList.remove('active_main');
+})
+
+a.forEach(function(e) {
+    e.addEventListener("click", () => {
+        navbar.classList.remove('active_navbar');
+        main.classList.remove('active_main');
+    })
+});
